1f52d20b5b2ad1db47603f75ed91caf562156396
a2ron-k <aaronkwah@gmail.com>
2023-03-11 16:25:14 +0800

    chore: add spring annotation

--- src/main/java/com/oop/leap_ahead_x/repos/FormWorkflowRepository.java
+++ src/main/java/com/oop/leap_ahead_x/repos/FormWorkflowRepository.java
@@ -3,8 +3,9 @@ package com.oop.leap_ahead_x.repos;
 import com.oop.leap_ahead_x.domain.FormWorkflow;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface FormWorkflowRepository extends JpaRepository<FormWorkflow, UUID> {
 
     boolean existsByNameIgnoreCase(String name);
--- src/main/java/com/oop/leap_ahead_x/repos/SubformCanvasRepository.java
+++ src/main/java/com/oop/leap_ahead_x/repos/SubformCanvasRepository.java
@@ -3,7 +3,8 @@ package com.oop.leap_ahead_x.repos;
 import com.oop.leap_ahead_x.domain.SubformCanvas;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface SubformCanvasRepository extends JpaRepository<SubformCanvas, UUID> {
 }
