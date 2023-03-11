0e0b747d1bf0ba027e05003f4d6177031f9d0109
HZKMaurice <zikaiho@gmail.com>
2023-03-12 00:53:28 +0800

    fix: get and post endpoints Application

--- src/main/java/com/oop/leap_ahead_x/service/ApplicationService.java
+++ src/main/java/com/oop/leap_ahead_x/service/ApplicationService.java
@@ -9,6 +9,7 @@ import com.oop.leap_ahead_x.repos.FormWorkflowRepository;
 import com.oop.leap_ahead_x.repos.VendorRepository;
 import com.oop.leap_ahead_x.exceptions.NotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -65,7 +66,9 @@ public class ApplicationService {
         applicationDTO.setStatus(application.getStatus());
         applicationDTO.setComment(application.getComment());
         applicationDTO.setCurrentStepNo(application.getCurrentStepNo());
-        applicationDTO.setCreatedFor(application.getCreatedFor() == null ? null : application.getCreatedFor().getVendorUuid());
+//        applicationDTO.setCreatedFor(application.getCreatedFor() == null ? null : application.getCreatedFor().getVendorUuid());
+        Optional<Vendor> vendor = vendorRepository.findById(application.getCreatedFor().getVendorUuid());
+        applicationDTO.setCompany(vendor.get().getCompany());
         applicationDTO.setFormUuid(application.getFormUuid() == null ? null : application.getFormUuid().getFormUuid());
         return applicationDTO;
     }
@@ -75,8 +78,11 @@ public class ApplicationService {
         application.setStatus(applicationDTO.getStatus());
         application.setComment(applicationDTO.getComment());
         application.setCurrentStepNo(applicationDTO.getCurrentStepNo());
-        final Vendor createdFor = applicationDTO.getCreatedFor() == null ? null : vendorRepository.findById(applicationDTO.getCreatedFor())
-                .orElseThrow(() -> new NotFoundException("createdFor not found"));
+//        final Vendor createdFor = applicationDTO.getCreatedFor() == null ? null : vendorRepository.findById(applicationDTO.getCreatedFor())
+//                .orElseThrow(() -> new NotFoundException("createdFor not found"));
+//        application.setCreatedFor(createdFor);
+
+        final Vendor createdFor = applicationDTO.getCompany() == null ? null : vendorRepository.findByCompanyName(applicationDTO.getCompany());
         application.setCreatedFor(createdFor);
         final FormWorkflow formUuid = applicationDTO.getFormUuid() == null ? null : formWorkflowRepository.findById(applicationDTO.getFormUuid())
                 .orElseThrow(() -> new NotFoundException("formUuid not found"));
