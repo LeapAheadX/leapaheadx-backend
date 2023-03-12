0f3e342b02f32fd8dea00843bf2d68368c1a0efb
Ray Quek <ray.quek@gmail.com>
2023-03-12 19:01:36 +0800

    feat:add customer query to JPA

--- src/main/java/com/oop/leap_ahead_x/repos/OptionsRepository.java
+++ src/main/java/com/oop/leap_ahead_x/repos/OptionsRepository.java
@@ -1,10 +1,16 @@
 package com.oop.leap_ahead_x.repos;
 
 import com.oop.leap_ahead_x.domain.Options;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OptionsRepository extends JpaRepository<Options, UUID> {
+    @Query("SELECT o FROM Options o JOIN o.optionComponentLinkInputComponents f WHERE f.componentUuid = :cId")
+    List<Options> findOptionsByComponent(@Param("cId") UUID cId);
 }
