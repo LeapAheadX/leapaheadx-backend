9e869092d2da76ccc767cb15eb7aca225de5b48a
HZKMaurice <zikaiho@gmail.com>
2023-03-13 23:44:47 +0800

    feat: new get endpoints for application

--- src/main/java/com/oop/leap_ahead_x/repos/ApplicationRepository.java
+++ src/main/java/com/oop/leap_ahead_x/repos/ApplicationRepository.java
@@ -1,10 +1,18 @@
 package com.oop.leap_ahead_x.repos;
 
 import com.oop.leap_ahead_x.domain.Application;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, UUID> {
+
+
+
+    List<Application> findByStatus(String status);
+
+
 }
