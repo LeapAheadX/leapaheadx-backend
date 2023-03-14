74d598225077b6508b4a6b5dc8feaf409de87ea0
Ray Quek <ray.quek@gmail.com>
2023-03-14 21:53:00 +0800

    chore:Resolve merge conflict

--- src/main/java/com/oop/leap_ahead_x/service/ApplicationService.java
+++ src/main/java/com/oop/leap_ahead_x/service/ApplicationService.java
@@ -5,6 +5,7 @@ import com.oop.leap_ahead_x.dto.ApplicationDTO;
 import com.oop.leap_ahead_x.repos.*;
 import com.oop.leap_ahead_x.exceptions.NotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import jakarta.transaction.Transactional;
@@ -271,28 +272,38 @@ public class ApplicationService {
         return ResponseEntity.ok(jsonString);
     }
 
-
     private ApplicationDTO mapToDTO(final Application application,
-            final ApplicationDTO applicationDTO) {
+                                    final ApplicationDTO applicationDTO) {
         applicationDTO.setApplicationUuid(application.getApplicationUuid());
         applicationDTO.setStatus(application.getStatus());
         applicationDTO.setComment(application.getComment());
         applicationDTO.setCurrentStepNo(application.getCurrentStepNo());
-        applicationDTO.setCreatedFor(application.getCreatedFor() == null ? null : application.getCreatedFor().getVendorUuid());
-        applicationDTO.setFormUuid(application.getFormUuid() == null ? null : application.getFormUuid().getFormUuid());
+//        applicationDTO.setCreatedFor(application.getCreatedFor() == null ? null : application.getCreatedFor().getVendorUuid());
+        Optional<Vendor> vendor = vendorRepository.findById(application.getCreatedFor().getVendorUuid());
+        applicationDTO.setCompany(vendor.get().getCompany());
+
+        Optional<FormWorkflow> formWorkflow = formWorkflowRepository.findById(application.getFormUuid().getFormUuid());
+        applicationDTO.setFormName(formWorkflow.get().getName());
+//        applicationDTO.setFormUuid(application.getFormUuid() == null ? null : application.getFormUuid().getFormUuid());
         return applicationDTO;
     }
 
     private Application mapToEntity(final ApplicationDTO applicationDTO,
-            final Application application) {
+                                    final Application application) {
         application.setStatus(applicationDTO.getStatus());
         application.setComment(applicationDTO.getComment());
         application.setCurrentStepNo(applicationDTO.getCurrentStepNo());
-        final Vendor createdFor = applicationDTO.getCreatedFor() == null ? null : vendorRepository.findById(applicationDTO.getCreatedFor())
-                .orElseThrow(() -> new NotFoundException("createdFor not found"));
+//        final Vendor createdFor = applicationDTO.getCreatedFor() == null ? null : vendorRepository.findById(applicationDTO.getCreatedFor())
+//                .orElseThrow(() -> new NotFoundException("createdFor not found"));
+//        application.setCreatedFor(createdFor);
+
+        final Vendor createdFor = applicationDTO.getCompany() == null ? null : vendorRepository.findByCompanyName(applicationDTO.getCompany());
         application.setCreatedFor(createdFor);
-        final FormWorkflow formUuid = applicationDTO.getFormUuid() == null ? null : formWorkflowRepository.findById(applicationDTO.getFormUuid())
-                .orElseThrow(() -> new NotFoundException("formUuid not found"));
+//        final FormWorkflow formUuid = applicationDTO.getFormUuid() == null ? null : formWorkflowRepository.findById(applicationDTO.getFormUuid())
+//                .orElseThrow(() -> new NotFoundException("formUuid not found"));
+//        application.setFormUuid(formUuid);
+
+        final FormWorkflow formUuid = applicationDTO.getFormName() == null ? null : formWorkflowRepository.findByFormName(applicationDTO.getFormName());
         application.setFormUuid(formUuid);
         return application;
     }
