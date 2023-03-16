fcc5a987b310c547efb74522a94d4a1ab3b3e85f
Ray Quek <ray.quek@gmail.com>
2023-03-16 19:44:58 +0800

    feat:add Delete API end point to remove response values using applicationID

--- src/main/java/com/oop/leap_ahead_x/controller/ApplicationResponseValueController.java
+++ src/main/java/com/oop/leap_ahead_x/controller/ApplicationResponseValueController.java
@@ -5,19 +5,16 @@ import com.oop.leap_ahead_x.service.ApplicationResponseValueService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/api/applicationResponseValues", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ApplicationResponseValueController {
 
@@ -39,6 +36,7 @@ public class ApplicationResponseValueController {
 //        return ResponseEntity.ok(applicationResponseValueService.get(responseId));
 //    }
 
+    //post/overwrite the response
     @PostMapping("/saveSpecificResponse")
     @ApiResponse(responseCode = "201")
     public ResponseEntity<String> createResponse(@RequestBody @Valid final List<ApplicationResponseValueDTO> responseDTO) {
@@ -47,12 +45,22 @@ public class ApplicationResponseValueController {
         return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 
+    //delete response using applicationID
+    @DeleteMapping("/application/{applicationId}")
+    @ApiResponse(responseCode = "204")
+    public ResponseEntity<Void> deleteApplicationResponseValue(
+            @PathVariable(name = "applicationId") final UUID aId) {
+        applicationResponseValueService.deleteByAId(aId);
+        return ResponseEntity.noContent().build();
+    }
+
 //    @PostMapping
 //    @ApiResponse(responseCode = "201")
 //    public ResponseEntity<Integer> createApplicationResponseValue(
 //            @RequestBody @Valid final ApplicationResponseValueDTO applicationResponseValueDTO) {
 //        return new ResponseEntity<>(applicationResponseValueService.create(applicationResponseValueDTO), HttpStatus.CREATED);
 //    }
+
 
     @PutMapping("/{responseId}")
     public ResponseEntity<Void> updateApplicationResponseValue(
