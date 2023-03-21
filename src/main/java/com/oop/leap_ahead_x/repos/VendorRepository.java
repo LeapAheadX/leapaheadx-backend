46de59b5b779be0db52c922230f6ffd8f2d5afef
a2ron-k <aaronkwah@gmail.com>
2023-03-21 16:38:39 +0800

    feat: add login function with endpoint

--- src/main/java/com/oop/leap_ahead_x/repos/VendorRepository.java
+++ src/main/java/com/oop/leap_ahead_x/repos/VendorRepository.java
@@ -1,6 +1,10 @@
 package com.oop.leap_ahead_x.repos;
 
+
+import com.oop.leap_ahead_x.domain.User;
 import com.oop.leap_ahead_x.domain.Vendor;
+
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +17,6 @@ public interface VendorRepository extends JpaRepository<Vendor, UUID> {
     @Async
     @Query ("SELECT vendor from Vendor vendor where vendor.company= :company")
     Vendor findByCompanyName(String company);
+
+    Vendor findByuId(Optional<User> uid);
 }
