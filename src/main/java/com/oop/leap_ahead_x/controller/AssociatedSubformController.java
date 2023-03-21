5fbb81eb90e145c1646587cd3373db5508713056
Ray Quek <ray.quek@gmail.com>
2023-03-21 21:24:19 +0800

    feat:add CROSS ORIGIN to controllers

--- src/main/java/com/oop/leap_ahead_x/controller/AssociatedSubformController.java
+++ src/main/java/com/oop/leap_ahead_x/controller/AssociatedSubformController.java
@@ -4,12 +4,14 @@ import com.oop.leap_ahead_x.dto.AssociatedSubformDTO;
 import com.oop.leap_ahead_x.service.AssociatedSubformService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/associatedSubform", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AssociatedSubformController {
