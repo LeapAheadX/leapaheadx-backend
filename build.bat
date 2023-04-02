138cb04d6233ab11c5766780e38d58117f9c9b47
a2ron-k <aaronkwah@gmail.com>
2023-04-02 22:47:09 +0800

    feat: add build scripts

--- build.bat
+++ build.bat
@@ -6,14 +6,8 @@ rmdir /s /q bin
 REM Create a new bin/ directory
 mkdir bin
 
-REM Run the SQL script to create the database
-mysql -h hostname -u username -ppassword < path/to/create_database.sql
-
 REM Build the project with Maven
 mvn clean install
 
 REM Copy the jar file to the bin/ directory
 copy target\*.jar bin\
-
-REM Run the SQL script to populate the database
-mysql -h hostname -u username -ppassword database_name < path/to/populate_database.sql
